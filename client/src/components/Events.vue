1e45f88f85f0e3452618c3714d36e32b48706fb2
pcm2798 <pcollet@edu.tecnocampus.cat>
2019-02-10 01:01:34 +0100

    filtratge

--- client/src/components/Events.vue
+++ client/src/components/Events.vue
@@ -2,27 +2,55 @@
   <section class="hero is-fullheight">
     <div class="hero-head1">
       <header class="navbar bg-nav is-transparent">
-          <img src="../assets/logo_nostre.png" class="logo_nostre">
+        <img src="../assets/logo_nostre.png" class="logo_nostre">
       </header>
     </div>
     <div class="hero-body fons-mataro">
       <div class="container has-text-centered">
-        <h1 class="title has-text-white">Esdeveniments
-          <router-link to="/event/new">
-            <a class="button is-rounded crear-event">
-              <span class="icon is-small">
-                <i class="fas fa-plus"></i>
-              </span>
-            </a>
-          </router-link>
-        </h1>
-        <h2 class="subtitle has-text-white">crear esdeveniment</h2>
+        <div class="columns is-mobile">
+          <div class="column column is-half is-offset-one-quarter has-text-centered">
+            <h1 class="title has-text-white">Esdeveniments
+              <router-link to="/event/new">
+                <a class="button is-rounded crear-event">
+                  <span class="icon is-small">
+                    <i class="fas fa-plus"></i>
+                  </span>
+                </a>
+              </router-link>
+            </h1>
+            <h2 class="subtitle has-text-white">crear esdeveniment</h2>
+          </div>
+          <div class="column is-4 is offset-8">
+            <div class="control has-text-white">
+              <p style="font-size: 20px">Filtrar per:</p>
+              <div class="select">
+                <select v-model="seleccio">
+                  <option value="tot">Tot</option>
+                  <option value="esports">Esports</option>
+                  <option value="cultural">Cultural</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
         {{ $store.state.count }}
         <div class="columns is-multiline is-mobile">
           <div class="column is-4" v-for="event in events" v-bind:key="event['_id']['$oid']">
-            <router-link v-bind:to="'/event/'+event['_id']['$oid']">
-              <event-card v-bind:event="event"></event-card>
-            </router-link>
+            <div class="field" v-if="seleccio == 'esports' && event.ambit == 'Esports'">
+              <router-link v-bind:to="'/event/'+event['_id']['$oid']">
+                <event-card v-bind:event="event"></event-card>
+              </router-link>
+            </div>
+            <div class="field" v-if="seleccio == 'cultural' && event.ambit =='Cultural'">
+              <router-link v-bind:to="'/event/'+event['_id']['$oid']">
+                <event-card v-bind:event="event"></event-card>
+              </router-link>
+            </div>
+            <div class="field" v-if="seleccio == 'tot'">
+              <router-link v-bind:to="'/event/'+event['_id']['$oid']">
+                <event-card v-bind:event="event"></event-card>
+              </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -33,8 +61,8 @@
 <script>
 import EventCard from "@/components/EventCard";
 import EventDetail from "@/components/EventDetail";
-import { mapGetters, mapActions } from 'vuex'
-import store from '../store'
+import { mapGetters, mapActions } from "vuex";
+import store from "../store";
 export default {
   name: "Events",
   components: {
@@ -42,11 +70,13 @@ export default {
     EventDetail
   },
   data() {
-    return {};
+    return {
+      seleccio: "tot"
+    };
   },
   computed: {
-    events () {
-      return store.state.events
+    events() {
+      return store.state.events;
     }
   }
 };
@@ -54,9 +84,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.crear-event{
+.crear-event {
   color: white;
-  background-color:#002f6c;
+  background-color: #002f6c;
 }
 .logo_nostre {
   width: 90px;
@@ -74,7 +104,8 @@ export default {
   justify-content: center;
 }
 .bg-nav {
-  background-color: #FFC107;
+  background-color: #ff8c00;
+  /* background-color: #FFC107; */
   height: 65px;
 }
 </style>
